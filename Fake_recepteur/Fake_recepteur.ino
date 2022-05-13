2b0a7a18fa025c5ee18e20165385ac63b217745a
yoryd <yoryyy@live.fr>
2022-05-13 09:13:51 +0200

    Update Fake_recepteur.ino

--- Fake_recepteur/Fake_recepteur.ino
+++ Fake_recepteur/Fake_recepteur.ino
@@ -1,12 +1,7 @@
 #include  <SPI.h>
-#include  <RF24.h>
 #include "packet.h"
 
 int tour = 0;
-
-RF24 radio(9,10); 
-uint8_t address[6] = {0x1E,0xCE,0xCC,0xCE,0xCC};   // Adresse du pipe
-payload_t rcv_payload;
 
 unsigned long refLat;
 unsigned long refLong;
@@ -17,14 +12,7 @@ void setup() {
 
   refLat = 45678490;
   refLong = 4526759;
-  
-  radio.begin();
-  radio.setChannel(30);
-  radio.setDataRate(RF24_2MBPS);
 
-  radio.openReadingPipe(0,address); // Ouvrir le Pipe en lecture
-  radio.startListening();
-  radio.enableDynamicPayloads();
 }
 
 void loop(void) {
@@ -32,12 +20,6 @@ void loop(void) {
     boolean timeout = false;
     
     if (millis() > 1000*tour) {
-        // On lit le payload
-        radio.read(&rcv_payload, sizeof(rcv_payload));
-
-        // On recalcule les latitude/longitude absolues grâce aux delta reçus
-        refLat = (unsigned long) (refLat + rcv_payload.deltaLat);
-        refLong = (unsigned long) (refLong + rcv_payload.deltaLong);
 
         // On écrit dans le moniteur les valeurs reçues
         Serial.println();

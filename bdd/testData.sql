3a3b734c534ae8cec45ef2b964f52a6009422d2a
yoryd <yoryyy@live.fr>
2022-05-17 14:59:45 +0200

    Update testData.sql

--- bdd/testData.sql
+++ bdd/testData.sql
@@ -1,8 +1,5 @@
-INSERT INTO Utilisateur (login, mdpUtilisateur)
-VALUES ("test", "test")
+INSERT INTO Utilisateur (login, mdpUtilisateur) VALUES ("test", "test");
 
-INSERT INTO Seance (dateDebut, idUtilisateur)
-VALUES ("2022-05-03", 1)
+INSERT INTO Seance (dateDebut, idUtilisateur) VALUES ("2022-05-03", 1);
 
-INSERT INTO Mesure (dateMesure,freqCardiaque,positionX,positionY,nbPas,idSeance)
-VALUES ("2022-05-03",200,1.55,3.6,3000,1)
+INSERT INTO Mesure (dateMesure,freqCardiaque,positionX,positionY,nbPas,idSeance) VALUES ("2022-05-03", 200, 1.55, 3.6, 3000, 1);

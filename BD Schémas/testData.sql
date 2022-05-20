4db68201bb68ae39a19aa616c8d37558de6b0515
YL <99618877+IgrecL@users.noreply.github.com>
2022-05-20 12:42:59 +0000

    Rename bdd to BD

---
+++ BD Schémas/testData.sql
@@ -0,0 +1,5 @@
+INSERT INTO Utilisateur (login, mdpUtilisateur) VALUES ("test", "test");
+
+INSERT INTO Seance (dateDebut, idUtilisateur) VALUES ("2022-05-03", 1);
+
+INSERT INTO Mesure (dateMesure,freqCardiaque,latitude,longitude,nbPas,idSeance) VALUES ("2022-05-03", 200, 1.55, 3.6, 3000, 1);

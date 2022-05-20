32593c6013e7c60417bf5cb51436a3b12883c288
AsticotSoyeuxx <ahmed.bulut@insa-lyon.fr>
2022-05-20 11:38:08 +0200

    phobie

--- Interface/Main.java
+++ Interface/Main.java
@@ -2,9 +2,6 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        DisplayImage test = new DisplayImage();
         Window myWindow1=new Window();
-
-        // Maps map = new Maps();
     }
 }

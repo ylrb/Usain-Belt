dbe0f6ade4bf8d910e5f18987a4ed2a2d7bff4b1
AsticotSoyeuxx <ahmed.bulut@insa-lyon.fr>
2022-05-20 11:31:26 +0200

    formaliser maps

--- Interface/Maps.java
+++ Interface/Maps.java
@@ -10,19 +10,20 @@ public class Maps {
     // Les variables qui vont changer selon les donnees gps sauf zoom a definir.
     public double lon = 4.874785;
     public double lat = 45.78288;
-    public int zoom = 16;
 
-    // Les bouts de l'url qui ne changent pas.
-    public final String debut = "https://maps.geoapify.com/v1/staticmap?style=osm-carto&width=500&height=400";
-    private final String apiKey = "f02d87f54d4c417db2f6eca959996a0a";
+    private final int ZOOM = 16;
+    private final int WIDTH = 640;
+    private final int HEIGHT = 420;
+    private final String START = "https://maps.geoapify.com/v1/staticmap?style=osm-carto";
+    private final String API_KEY = "f02d87f54d4c417db2f6eca959996a0a";
 
     // L'url en string.
-    public String urlString;
+    private String urlString;
 
     public Maps() throws IOException {
 
         // Url en string
-        urlString = debut + "&center=lonlat:" + lon + "," + lat + "&zoom=" + zoom + "&apiKey=" + apiKey;
+        urlString = START + "&width=" + WIDTH + "&height=" + HEIGHT + "&center=lonlat:" + lon + "," + lat + "&zoom=" + ZOOM + "&apiKey=" + API_KEY;
         System.out.println(urlString);
 
         // Recupere l'image depuis l'url.

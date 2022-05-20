4cf67696e0ff1c4ab749eaebec06d7a92d971fd1
YL <99618877+IgrecL@users.noreply.github.com>
2022-05-20 12:44:13 +0000

    Add files via upload

---
+++ Arduino-TC/Fake_recepteur/packet.h
@@ -0,0 +1,17 @@
+#ifndef __PACKET_H__
+#define __PACKET_H__
+
+typedef struct payload_t
+{
+  uint8_t src_node;
+  uint8_t dest_node;
+  uint16_t pkt_id;
+  bool type;
+  unsigned long time;
+  unsigned short pas;
+  unsigned char bpm;
+  unsigned short deltaLat;
+  unsigned short deltaLong;
+} payload_t;
+
+#endif

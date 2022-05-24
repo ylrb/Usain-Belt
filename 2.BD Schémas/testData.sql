ebef0dfdaadf3bc429b19353ffb36c663ec2a56b
yoryd <yoryyy@live.fr>
2022-05-24 08:48:05 +0200

    Update testData.sql

--- 2.BD Schémas/testData.sql
+++ 2.BD Schémas/testData.sql
@@ -1,5 +1,10 @@
-INSERT INTO Utilisateur (login, mdpUtilisateur) VALUES ("test", "test");
+INSERT INTO Utilisateur (nom) VALUES ("Julien");
 
 INSERT INTO Seance (dateDebut, idUtilisateur) VALUES ("2022-05-03", 1);
 
-INSERT INTO Mesure (dateMesure,freqCardiaque,latitude,longitude,nbPas,idSeance) VALUES ("2022-05-03", 200, 1.55, 3.6, 3000, 1);
+INSERT INTO G223_A_BD1.Mesure (dateMesure, freqCardiaque, latitude, longitude, nbPas, idSeance, vitesseInstant) 
+	VALUES ('2022-05-03 13:40:00.0', 200, 45.78288, 4.874785, 3000, 1, NULL);
+INSERT INTO G223_A_BD1.Mesure (dateMesure, freqCardiaque, latitude, longitude, nbPas, idSeance, vitesseInstant) 
+	VALUES ('2022-05-03 13:40:01.0', 200, 45.78188, 4.874785, 3000, 1, NULL);
+
+# DISTANCE ENTRE LES DEUX POINTS EN HAUT = 140m

c4b6bf53de9821125f06246ef28cfe99a5bfe068
AsticotSoyeuxx <ahmed.bulut@insa-lyon.fr>
2022-05-24 10:37:25 +0200

    AA

--- 4.Interface/Main.java
+++ 4.Interface/Main.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        System.setProperty("awt.useSystemAAFontSettings","on");
         MainWindow main = new MainWindow();
     }
 }

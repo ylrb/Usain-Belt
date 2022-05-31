a795f0aea35d3c6183211e904a02e997c523d219
AsticotSoyeuxx <ahmed.bulut@insa-lyon.fr>
2022-05-31 11:05:31 +0200

    test map

--- 4.Interface/Maps.java
+++ 4.Interface/Maps.java
@@ -3,12 +3,18 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
+import java.util.LinkedList;
+import java.awt.*;
 
 public class Maps {
     
     // Les variables qui vont changer selon les donnees gps sauf zoom a definir.
-    public double lon = 4.874785;
-    public double lat = 45.78288;
+    public static double lon = 4.874785;
+    public static double lat = 45.78288;
+    public static LinkedList<Double> posLon = new LinkedList<Double>();
+    public static LinkedList<Double> posLat = new LinkedList<Double>();
+    public static int[] deltaLon = new int[200];
+    public static int[] deltaLat = new int[200];
     public static boolean internetAccess = true;
 
     private final int ZOOM = 16;
@@ -22,6 +28,16 @@ public class Maps {
 
     public Maps() throws IOException {
 
+        // Génère des valeurs aléatoires entre 0.00001 et 0.001 et remplit les LinkedList
+        for (int i=0; i<100; i++) {
+            posLon.add(Math.random()*0.001);
+            posLat.add(Math.random()*0.001);
+        }
+        for (int i=0; i<posLon.size(); i++) {
+            deltaLon[i] = (int) ((posLon.get(i)-lon)*(72/0.001));
+            deltaLat[i] = (int) ((posLat.get(i)-lat)*(104/0.001));
+        }
+
         // Url en string
         urlString = START + "&width=" + WIDTH + "&height=" + HEIGHT + "&center=lonlat:" + lon + "," + lat + "&zoom=" + ZOOM + "&apiKey=" + API_KEY;
         System.out.println(urlString);
@@ -31,11 +47,10 @@ public class Maps {
             URL url = new URL(urlString);
             BufferedImage img = ImageIO.read(url);
             File file = new File(System.getProperty("user.dir") + System.getProperty("file.separator")+ "4.Interface/assets/carte.jpg");
+
             ImageIO.write(img, "jpg", file);
         } catch (IOException io) {
             internetAccess = false;
         }
     }
-
-
 }
